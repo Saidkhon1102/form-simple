1fb42bc9ce7cad196a65cd4398bf82c7c589a8ef
Saidxon <saidxon@gmail.com>
2025-07-23 15:19:53 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
   return (
     <div className="center-container">
       <form onSubmit={handeSubmit}>
-        <div className="form-group">
+        <div className=" text-amber-200 form-group">
           <label htmlFor="">name</label>
           <input type="text" ref={name} />
         </div>
